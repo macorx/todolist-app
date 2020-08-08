77e31245dbec08ed2018f9d8ff4e1efda0bef745
Marco Antonio Correa <macorx@gmail.com>
2020-08-08 23:29:31 +0100

    - Adding additional case to acceptance test to sign out after seeing TodoItems view.

---
+++ TodoListApp.AcceptanceTests/AcceptanceTestBase.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TodoListApp.AcceptanceTests
+{
+    public class AcceptanceTestBase
+    {
+        protected IWebDriver Driver { get; private set; }
+
+        protected const string BasePath = "https://localhost:5001";
+        protected readonly string LoginUrl = $"{BasePath}/Identity/Account/Login";
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new FirefoxDriver(new FirefoxOptions() { AcceptInsecureCertificates = true });
+            Driver.Navigate().GoToUrl(LoginUrl);
+            Driver.WaitUntilPageIsReady();            
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+
+    }
+}
--- TodoListApp.AcceptanceTests/WhenUserSignsIn.cs
+++ TodoListApp.AcceptanceTests/WhenUserSignsIn.cs
@@ -1,40 +1,24 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 
 namespace TodoListApp.AcceptanceTests
 {
-    public class WhenUserSignsIn
+    public class WhenUserSignsIn : AcceptanceTestBase
     {
-        private IWebDriver driver;
-
-        private const string BasePath = "https://localhost:5001";
-
-        [SetUp]
-        public void Setup()
-        {
-            driver = new FirefoxDriver(new FirefoxOptions() { AcceptInsecureCertificates = true });
-            driver.Navigate().GoToUrl($"{BasePath}/Identity/Account/Login");
-            driver.WaitUntilPageIsReady();            
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            driver.Quit();
-            driver.Dispose();
-        }
-
         [Test]
-        public void DisplaysUsersTodoList()
+        public void DisplaysUsersTodoListAndSignsOut()
         {
-            driver.FindElement(By.CssSelector("input#UserName")).SendKeys("test");
-            driver.FindElement(By.CssSelector("input#Password")).SendKeys("pwd123");
-            driver.FindElement(By.CssSelector("button")).Submit();
+            Driver.FindElement(By.CssSelector("input#UserName")).SendKeys("test");
+            Driver.FindElement(By.CssSelector("input#Password")).SendKeys("pwd123");
+            Driver.FindElement(By.CssSelector("button#login")).Submit();
             
-            driver.WaitUntilPageIsReady();
+            Driver.WaitUntilPageIsReady();
             
-            Assert.That(driver.Url, Is.EqualTo($"{BasePath}/"));
+            Assert.That(Driver.Url, Is.EqualTo($"{BasePath}/"));
+            
+            Driver.FindElement(By.CssSelector("button#logout")).Submit();
+            
+            Assert.That(Driver.Url, Is.EqualTo(LoginUrl));            
         }
     }
 }
