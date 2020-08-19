6b375c7a3dd575ba90cb8eed994bbc8730be94f9
Marco Antonio Correa <macorx@gmail.com>
2020-08-19 10:43:59 +0100

    - Adding controller and view model for Admin page.

---
+++ TodoListApp.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TodoListApp.WebApp.Data;
+using TodoListApp.WebApp.Models;
+
+namespace TodoListApp.WebApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ITodoItemRepository todoItemRepository;
+
+        public AdminController(UserManager<IdentityUser> userManager, ITodoItemRepository todoItemRepository)
+        {
+            this.userManager = userManager;
+            this.todoItemRepository = todoItemRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var adminViewModel = new AdminViewModel
+            {
+                TotalUsers = userManager.Users.Count(),
+                TotalPendingTasks = todoItemRepository.GetAll().Count(t => !t.IsDone),
+                TotalCompletedTasks = todoItemRepository.GetAll().Count(t => t.IsDone),
+            };
+            
+            return View(adminViewModel);
+        }
+        
+    }
+}
---
+++ TodoListApp.WebApp/Models/AdminViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApp.WebApp.Models
+{
+    public class AdminViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalPendingTasks  { get; set; }
+        public int TotalCompletedTasks  { get; set; }
+    }
+}
